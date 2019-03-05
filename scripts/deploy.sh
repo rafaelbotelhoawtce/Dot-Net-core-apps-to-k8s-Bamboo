8844db0bc1d2e30de31a74b52f92327ac89ea6ec
rafaelbotelhoawtce <48213524+rafaelbotelhoawtce@users.noreply.github.com>
2019-03-05 15:20:18 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,13 +10,13 @@ kubectl create ns test
 #kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n test
 
 #rm -rf helm
-/usr/local/bin/helm repo add helm $HELM_REPO
-/usr/local/bin/helm repo update
+helm repo add helm $HELM_REPO
+helm repo update
 
 #helm fetch http://23.101.135.43/artifactory/helm-local/devops-0.45.0.tgz
 
 #helm install
-/usr/local/bin/helm install --namespace test --name aw-application-latest-test --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
+helm install --namespace test --name aw-application-latest-test --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
 
 #helm upgrade
 #/usr/local/bin/helm upgrade --install --namespace test aw-application-latest-test --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
