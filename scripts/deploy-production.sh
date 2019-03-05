762239ef100d9e07c3df320e8bd967786e167e67
rafaelbotelhoawtce <48213524+rafaelbotelhoawtce@users.noreply.github.com>
2019-03-05 15:37:37 +0100

    Update deploy-production.sh

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -2,12 +2,11 @@
 
 HELM_REPO=$bamboo_HELM_REPO
 
-#create env if not present
 kubectl create ns production
 
-#kubectl run hello-dotnet --image=sugendh/dotnetcorebmb:latest --port=8080 -n production
-#sleep 60
-#kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n production
+kubectl run hello-dotnet --image=sugendh/dotnetcorebmb:latest --port=8080 -n production
+sleep 60
+kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n production
 
 #rm -rf helm
 helm repo add helm $HELM_REPO
@@ -17,6 +16,6 @@ helm repo update
 helm install --namespace production --name dotnetcorebmb-latest-prod --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
 
 #helm upgrade
-#helm upgrade --install --namespace production dotnetcorebmb-latest-prod --set image.repository=sugendh/dotnetcorebmb,image.tag=latest helm/devops
+helm upgrade --install --namespace production dotnetcorebmb-latest-prod --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
 
 kubectl run myapplication --labels="app=myapplication" --image=rafaelbotelhoawtce/hdc:latest --replicas=2 --port=8080
