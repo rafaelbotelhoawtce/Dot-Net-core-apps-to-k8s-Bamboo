e1142f4e89685de8deec680a080dcf56eebabb07
rafaelbotelhoawtce <48213524+rafaelbotelhoawtce@users.noreply.github.com>
2019-03-05 16:08:53 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ HELM_REPO=$bamboo_HELM_REPO
 #create env if not present
 kubectl create ns test
 
-#kubectl run hello-dotnet --image=sugendh/dotnetcorebmb:latest --port=8080 -n test
-#kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n test
+kubectl run myapplication --image=rafaelbotelhoawtce/hdc:latest --port=8080 -n test
+kubectl expose deployment myapplication --type=LoadBalancer --port=8080 -n test
 
 #rm -rf helm
 helm repo add helm $HELM_REPO
@@ -16,7 +16,7 @@ helm repo update
 #helm fetch http://23.101.135.43/artifactory/helm-local/devops-0.45.0.tgz
 
 #helm install
-helm install --namespace test --name aw-application-latest-test --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
+helm install --namespace test --name myapplication-test --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
 
 #helm upgrade
 #/usr/local/bin/helm upgrade --install --namespace test aw-application-latest-test --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
