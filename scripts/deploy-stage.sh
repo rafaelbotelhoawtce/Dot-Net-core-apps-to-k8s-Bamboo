becd8a1bfe6a5bab7f5c8c290a0557a331206f68
rafaelbotelhoawtce <48213524+rafaelbotelhoawtce@users.noreply.github.com>
2019-03-05 16:07:36 +0100

    Update deploy-stage.sh

--- scripts/deploy-stage.sh
+++ scripts/deploy-stage.sh
@@ -5,16 +5,16 @@ HELM_REPO=$bamboo_HELM_REPO
 #create env if not present
 kubectl create ns staging
 
-#kubectl run hello-dotnet --image=sugendh/dotnetcorebmb:latest --port=8080 -n staging
-#sleep 60
-#kubectl expose deployment hello-dotnet --type=LoadBalancer --port=8080 -n staging
+kubectl run myapplication --image=rafaelbotelhoawtce/hdc:latest --port=8080 -n staging
+sleep 60
+kubectl expose deployment myapplication --type=LoadBalancer --port=8080 -n staging
 
 #rm -rf helm
 helm repo add helm $HELM_REPO
 helm repo update
 
 #helm install
-helm install --namespace staging --name dotnetcorebmb-latest-stage --set image.repository==rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
+helm install --namespace staging --name myapplication-latest-stage --set image.repository==rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
 
 #helm upgrade
-#/usr/local/bin/helm upgrade --install --namespace staging dotnetcorebmb-latest-stage --set image.repository=sugendh/dotnetcorebmb,image.tag=latest helm/devops
+#helm upgrade --install --namespace staging myapplication-stage --set image.repository=rafaelbotelhoawtce/hdc,image.tag=latest helm/devops
